b6938c363d09a64a6acf05f0ae02fa53d5b760a8
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-05 16:25:41 -0300

    fix indentação js

--- Calculadora v3/script.js
+++ Calculadora v3/script.js
@@ -5,40 +5,48 @@ function resultado() {
   tensao = document.getElementById("tensao").value;
   corrente = document.getElementById("corrente").value;
 
-  //validacao  
-  if (corrente == "" && tensao == "" && resistencia == "" || corrente == "" && resistencia == "" || corrente == "" && tensao == "" || tensao == "" && resistencia == "") {
+  //validacao
+  if (
+    (corrente == "" && tensao == "" && resistencia == "") ||
+    (corrente == "" && resistencia == "") ||
+    (corrente == "" && tensao == "") ||
+    (tensao == "" && resistencia == "")
+  ) {
     alert("ERRO! Digite no mínimo dois valores para realizar o cálculo");
-    return
-  }
-
-  else if (corrente != "" && tensao != "" && resistencia != "") {
+    return;
+  } else if (corrente != "" && tensao != "" && resistencia != "") {
     alert("Digite apenas dois valores realizar o cálculo!");
-    return
-}
+    return;
+  }
 
   //calculo corrente
   if (document.getElementById("corrente").value == "") {
-    document.getElementById("resultado").innerHTML = `Corrente = ${tensao / resistencia} A (amperes)`;
-    definirValores(resistencia, tensao, corrente)
+    document.getElementById("resultado").innerHTML = `Corrente = ${
+      tensao / resistencia
+    } A (amperes)`;
+    definirValores(resistencia, tensao, corrente);
     document.getElementById("corrente").value = tensao / resistencia;
   }
 
   //calculo tensao
   else if (document.getElementById("tensao").value == "") {
-    document.getElementById("resultado").innerHTML = `Tensão = ${resistencia * corrente} V (volts)`;
-    definirValores(resistencia, tensao, corrente)
+    document.getElementById("resultado").innerHTML = `Tensão = ${
+      resistencia * corrente
+    } V (volts)`;
+    definirValores(resistencia, tensao, corrente);
     document.getElementById("tensao").value = resistencia * corrente;
   }
 
   //calculo resistencia
   else if (document.getElementById("resistencia").value == "") {
-    document.getElementById("resultado").innerHTML = `Resistência = ${tensao / corrente} Ω (ohms)`;
-    definirValores(resistencia, tensao, corrente)
+    document.getElementById("resultado").innerHTML = `Resistência = ${
+      tensao / corrente
+    } Ω (ohms)`;
+    definirValores(resistencia, tensao, corrente);
     document.getElementById("resistencia").value = tensao / corrente;
-  } 
-  
+  }
 }
-function definirValores(resistencia, tensao, corrente){
+function definirValores(resistencia, tensao, corrente) {
   document.getElementById("resistencia").value = resistencia;
   document.getElementById("tensao").value = tensao;
   document.getElementById("corrente").value = corrente;
